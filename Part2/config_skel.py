cb77b8dc1b71d272da589148ea56b1ed7e23d337
nikohernandiz <nikohernandiz@gmail.com>
2016-11-16 10:25:15 -0800

    Update config_skel.py

--- Part2/config_skel.py
+++ Part2/config_skel.py
@@ -3,5 +3,5 @@
 
 # Fill in the values and rename this file to config_bot.py
 
-REDDIT_USERNAME = ''  # YOUR USERNAME as string
-REDDIT_PASS = ''  # YOUR PASSWORD as string
+REDDIT_USERNAME = 'QuoteGetter'  # YOUR USERNAME as string
+REDDIT_PASS = 'Legends91'  # YOUR PASSWORD as string
